d3be667f5b69177145e6c606e6066a9f6d1f12be
harbs0626 <harbinramo@yahoo.com>
2019-07-08 14:21:10 -0400

    Added codes for Calculator Button Click

--- COMP123-S2019-Lesson9B/CalculatorForm.cs
+++ COMP123-S2019-Lesson9B/CalculatorForm.cs
@@ -12,9 +12,35 @@ namespace COMP123_S2019_Lesson9B
 {
     public partial class CalculatorForm : Form
     {
+        /// <summary>
+        /// This is the constructor for the Calculator Form
+        /// </summary>
         public CalculatorForm()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// This is the shared event handler for all the Calculator buttons Click Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculatorButton_Click(object sender, EventArgs e)
+        {
+            var TheButton = sender as Button;
+
+            int buttonValue;
+            bool resultCondition = int.TryParse(TheButton.Text, out buttonValue);
+
+            if (resultCondition)
+            {
+                ResultLabel.Text += buttonValue;
+            }
+            else
+            {
+                ResultLabel.Text = "Not A Number (NaN)";
+            }
+            
         }
     }
 }
